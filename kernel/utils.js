45b1db44b6caaea55fc46b34c6d714346ca3b24f
yegabebar <m.barbegayet@gmail.com>
2022-02-24 23:03:24 +0100

    Premier jet de code pour création de transaction sur node bigchaindb

--- kernel/utils.js
+++ kernel/utils.js
@@ -1,34 +1,84 @@
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-//const baseController = require('../controllers/baseController')
+const driver = require('bigchaindb-driver');
+const jwt = require('jsonwebtoken');
+const {uri} = require('../kernel/db');
+const {Ed25519Sha256} = require('crypto-conditions');
+const res = require('express/lib/response');
+// const baseController = require('../controllers/baseController')
 
-module.exports.jwtKey = 'my_secret_key'
-/* module.exports.defaultAdmin = {
-    admUsername: 'origin',
-    admPasswd: 'h4xx0rz!'
-} */
+// const identity = new driver.Ed25519Keypair()
+module.exports.jwtKey = 'my_secret_key';
 
+/* module.exports.keys = {
+  'publicKey':'B9Y5QTx89DzKbaefD9PWFKvAdt91shhYDCjGK2oY5Mc4',
+  'privateKey':'GHKczvHhvKVoqHx153MX28c9sG5QiVsGuKxCxXZgefbH'
+}; */
+
+// Token
 module.exports.verifyToken = (res, token) => {
-    try {
-        return jwt.verify(token, this.jwtKey)
-    } catch (e) {
-        if (e instanceof jwt.JsonWebTokenError) {
-            return res.status(401).end()
-        }
-        return res.status(400).end()
+  try {
+    return jwt.verify(token, this.jwtKey);
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      return res.status(401).end();
     }
+    return res.status(400).end();
+  }
+};
+
+
+// Transactions
+
+module.exports.createNewAsset = async (data, metadata) => {
+  const {publicKey, privateKey} = this.generateKeyPair();
+  const tx = this.createTx(data, metadata, publicKey);
+  const signedTx = this.signTx(tx, privateKey);
+
+  return this.postTx(signedTx);
 }
 
+module.exports.generateKeyPair = () => {
+  return new driver.Ed25519Keypair()
+};
+
+module.exports.createTx = (data, metadata, publicKey) => {
+  const output = driver.Transaction.makeOutput(
+    driver.Transaction.makeEd25519Condition(publicKey)
+  );
+  return driver.Transaction.makeCreateTransaction(
+      data,
+      metadata,
+      [output],
+      publicKey,
+  );
+};
+
+module.exports.signTx = (transaction, privateKey) => {
+  return driver.Transaction.signTransaction(transaction, privateKey);
+};
+
+module.exports.postTx = async (signedTransaction) => {
+  console.log('got uri', uri)
+  const conn = new driver.Connection('http://localhost:9984/api/v1/');
+  console.log('conn', conn)
+  //const retrievedTx = await conn.postTransactionSync(signedTransaction);
+  return conn.postTransactionCommit(signedTransaction)
+  .then(resp => resp)
+  .catch(err => {
+    console.log('Error', err)
+  });
+};
+
+// Misc
 module.exports.getModelProperties = (modelName, exclusions) => {
-	const newModel = mongoose.model(modelName)
-	let properties = []
-	newModel.schema.eachPath(property => properties.push(property))
-	return properties.filter((value) => {
-		return !exclusions.includes(value) ? value : undefined
-	})
-}
+  const newModel = mongoose.model(modelName);
+  const properties = [];
+  newModel.schema.eachPath((property) => properties.push(property));
+  return properties.filter((value) => {
+    return !exclusions.includes(value) ? value : undefined;
+  });
+};
 
-/* Non utilisé, laissé à titre d'exemple pour montrer syntaxes possibles lorsque l'on 
+/* Non utilisé, laissé à titre d'exemple pour montrer syntaxes possibles lorsque l'on
 a besoin d'utiliser des controllers et appliquer des traitements suite au résultat obtenu */
 /* module.exports.accessGranted = async(token, resourceType) => {
 	const decodedToken = jwt.verify(token, this.jwtKey)
@@ -45,7 +95,7 @@ a besoin d'utiliser des controllers et appliquer des traitements suite au résul
 		if(resp.status==200){
 			resp.data[0].permissionsId.filter((permId)=>{ permissionsId = { _id: permId }})
 		}
-	}) 
+	})
 	return await baseController.readDocuments(permissionsId, 'Permission', ['resourceName','accessRight','__v']).then(resp => {
 		// Si le rôle comporte une permission All (Permission donnant accès à toute ressource), on return la valeur
 		if(resp.status == 200 && resp.data[0].resourceName == "All"){
@@ -54,5 +104,5 @@ a besoin d'utiliser des controllers et appliquer des traitements suite au résul
 		if(resp.status == 200 && resp.data[0].resourceName == resourceType){
 			return resp.data[0].accessRight
 		}
-	}) 
+	})
 } */
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,83 +1,95 @@
-const express = require("express")
-const router = express.Router()
-const utils = require('../kernel/utils')
+const express = require('express');
+const router = express.Router();
+const utils = require('../kernel/utils');
 const { success, error, validation } = require("../kernel/formatResponse");
 
 router.get('/', (req, res) => {
-    res.write('Envoie une requête pour créer un nouvel article')
-    res.write(`
+  res.write('Envoie une requête pour créer un nouvel article');
+  res.write(`
         Pour tester l'API, utiliser Postman, Insomnia ou autre moyen
         permettant d'inclure un body avec les requetes HTTP.
-    `)
-    res.send()
-})
+    `);
+  res.send();
+});
 
-router.post('/create', async(req, res) => {
-    const token = req.cookies.token
-    try{
-        const payload = utils.verifyToken(res, token)
-        if(payload.status){ 
-            res.end()
-            return payload.status 
-        }
-    }catch(e){
-        return res.status(401).end()
+router.post('/create', async (req, res) => {
+  const token = req.cookies.token;
+  try {
+    const payload = utils.verifyToken(res, token);
+    if (payload.status) {
+      res.end();
+      return payload.status;
     }
-    // TODO: appeler un controller pour stocker des données dans BigchainDB
-    
-    // TODO: add orbitDB create operation to store  and check the result here
-    
-    // exemple d'implémentation de réponse de controller, permet de retourner un statut cohérent avec le résultat
-    // rendu par le controller.
-    /* baseController.createDocument(req.body, resource, ['_id','__v'], titleAndImageSchema).then(resp => {
+  } catch (e) {
+    return res.status(401).end();
+  }
+  // TODO: appeler un controller pour stocker des données dans BigchainDB
+  // Ajouter gestion d'erreurs
+  await utils.createNewAsset(req.body.data, req.body.metadata).then(resp => {
+    console.log('resp!',resp)
+    if(resp.id){
+      res.write(resp.id)
+      res.status(200);
+    }
+  }).catch(err => {
+    console.log('resp!', err)
+    res.write(err);
+    res.status(500);
+  })
+  res.send();
+  // TODO: add orbitDB create operation to store  and check the result here
+
+  // exemple d'implémentation de réponse de controller, permet de retourner un statut cohérent avec le résultat
+  // rendu par le controller.
+  /* baseController.createDocument(req.body, resource, ['_id','__v'], titleAndImageSchema).then(resp => {
         if(resp.statusMessage){ res.statusMessage = resp.statusMessage }
         if(resp.data){(res.write(JSON.stringify(resp.data)))}
         res.status(Number(resp.status))
         res.send()
     }) */
-})
+  
+  
+});
 
-router.post('/read', async(req, res) => {
-    const token = req.cookies.token
-    try{
-        const payload = utils.verifyToken(res, token)
-        if(payload.status){ 
-            res.end()
-            return payload.status 
-        }
-    }catch(e){
-        return res.status(401).end()
+router.post('/read', async (req, res) => {
+  const token = req.cookies.token;
+  try {
+    const payload = utils.verifyToken(res, token);
+    if (payload.status) {
+      res.end();
+      return payload.status;
     }
-    // TODO: appeler un controller pour lire des données dans BigchainDB
-    
-    // TODO: add orbitDB read operation to store and check the result here
-    
-    // exemple d'implémentation de réponse de controller, permet de retourner un statut cohérent avec le résultat
-    // rendu par le controller.
-    /* baseController.createDocument(req.body, resource, ['_id','__v'], titleAndImageSchema).then(resp => {
+  } catch (e) {
+    return res.status(401).end();
+  }
+  // TODO: appeler un controller pour lire des données dans BigchainDB
+
+  // TODO: add orbitDB read operation to store and check the result here
+
+  // exemple d'implémentation de réponse de controller, permet de retourner un statut cohérent avec le résultat
+  // rendu par le controller.
+  /* baseController.createDocument(req.body, resource, ['_id','__v'], titleAndImageSchema).then(resp => {
         if(resp.statusMessage){ res.statusMessage = resp.statusMessage }
         if(resp.data){(res.write(JSON.stringify(resp.data)))}
         res.status(Number(resp.status))
         res.send()
     }) */
-})
+});
 
-router.post('/update', async(req, res) =>{
-    const token = req.cookies.token
-    try{
-        const payload = utils.verifyToken(res, token)
-        if(payload.status){ 
-            res.end()
-            return payload.status 
-        }
-    }catch(e){
-        return res.status(401).end()
+router.post('/update', async (req, res) =>{
+  const token = req.cookies.token;
+  try {
+    const payload = utils.verifyToken(res, token);
+    if (payload.status) {
+      res.end();
+      return payload.status;
     }
-    // Va permettre de mettre à jour le score 
-    /* TODO: appeler un controller pour stocker des données dans BigchainDB (pas besoin de orbitdb et IPFS 
+  } catch (e) {
+    return res.status(401).end();
+  }
+  // Va permettre de mettre à jour le score
+  /* TODO: appeler un controller pour stocker des données dans BigchainDB (pas besoin de orbitdb et IPFS
     dans ce cas car les données stockées sur IPFS ne seront pas modifiables après création) */
-    
+});
 
-})
-
-module.exports = router
+module.exports = router;
