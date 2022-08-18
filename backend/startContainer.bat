7d4a8d37ef8ec38c0da0373106a020af76e41334
MathieuBarbeGayet <mathieu.barbe-gayet@estiam.com>
2022-08-18 15:46:06 +0200

    Fixed the volume creation that wasn't working when triggered by the npm script

--- backend/startContainer.bat
+++ backend/startContainer.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run --detach --name bigchaindb --publish 9984:9984 --publish 9985:9985 --publish 27017:27017 --publish 26657:26657 --volume ~/bigchaindb_docker/mongodb/data/db:/data/db --volume ~/bigchaindb_docker/mongodb/data/configdb:/data/configdb --volume ~/bigchaindb_docker/tendermint:/tendermint bigchaindb/bigchaindb:all-in-one
+docker run --detach --name bigchaindb --publish 9984:9984 --publish 9985:9985 --publish 27017:27017 --publish 26657:26657 --volume ./db:/data/db --volume ./configdb:/data/configdb --volume ./tendermint:/tendermint bigchaindb/bigchaindb:all-in-one
